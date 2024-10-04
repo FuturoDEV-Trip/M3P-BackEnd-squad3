a3b59c7064dcc30bf6f4ad957f11103303a02171
rayssa-freitas <rayssa_mf_lima@estudante.sesisenai.org.br>
2024-10-03 21:35:51 -0300

    alteracoes de endereco

--- src/routes/usuarios.route.js
+++ src/routes/usuarios.route.js
@@ -16,10 +16,29 @@ usuarioRoutes.post('/cadastrar', UsuarioController.cadastrar
         cpf: "78398747283",
         email: "rayssafreitas102@gmail.com",
         senha: "Rkd90#!js",
-        bairro: "Centro",
+        endereco: "Centro",
         data_nascimento: "2001-01-24",
         sexo: "Feminino"
      }
+           {
+    id: 2,
+    nome: "Manoel dos Santos",
+    cpf: "94738189037",
+    email: "manoel.santos@gmail.com",
+    senha: "MS674!msan",
+    endereco: "Córrego Grande",
+    data_nascimento: "1978-06-18",
+    sexo: "Masculino"
+   },
+   {
+    nome: "Manoela Pereira de Jesus",
+    cpf: "63819278647",
+    email: "manupereira@gmail.com",
+    senha: "Jesuspe67!",
+    endereco: "Vargem Grande",
+    data_nascimento: "1997-03-20",
+    sexo: "Feminino"
+   }
     }
    */
    )
