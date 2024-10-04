1661a58fc53980da5dded919bc093715b1e622b5
rayssa-freitas <rayssa_mf_lima@estudante.sesisenai.org.br>
2024-10-03 21:52:34 -0300

    atualizando para incluir requisicoes a mais de endereco

--- src/services/cep.service.js
+++ src/services/cep.service.js
@@ -3,10 +3,15 @@ const axios = require('axios')
 const buscaEnderecoCep = async (cep) => {
     try{
         const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-        return response.data
-    } catch {
-        throw new error("Erro ao buscar endereço. Tente novamente.")
-    }
+        
+        if (response.data.erro) {
+        throw new error("CEP inválido")
+    } 
+    const {logradouro, local:cidade, uf:estado} = response.data;
+    return{logradouro, cidade, estado, cep};
+} catch (error) {
+    throw new error("Erro ao buscar endereço. Tente novamente.")
 }
+
 
 MediaSourceHandle.exports = buscaEnderecoCep
