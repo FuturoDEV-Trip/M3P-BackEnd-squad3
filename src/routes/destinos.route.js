8b0e7f47bba5b20c63003fb2be8f29ed5afd6061
Roni <ronisank21@hotmail.com>
2024-10-06 22:05:46 -0300

    FIX endpoint cadastrar destino

--- src/routes/destinos.route.js
+++ src/routes/destinos.route.js
@@ -6,7 +6,7 @@ const destinoRoutes = new Router();
 destinoRoutes.post('/', DestinoController.cadastrar);
 destinoRoutes.put('/:id', DestinoController.atualizar);
 destinoRoutes.get('/:usuario_id', DestinoController.listarPorId);
-destinoRoutes.get('/usuario', DestinoController.consultar);
-destinoRoutes.delete('/:id',  DestinoController.excluir);
+destinoRoutes.get('/', DestinoController.consultar);
+destinoRoutes.delete('/:id', DestinoController.excluir);
 
 module.exports = destinoRoutes;
