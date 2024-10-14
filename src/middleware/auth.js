d62128144bace7e089621456f579ea0d23224b4b
rayssa-freitas <rayssa_mf_lima@estudante.sesisenai.org.br>
2024-10-13 21:54:15 -0300

    docker atualizado

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken')
 async function auth(req, res, next) {
    try {
 
-<<<<<<< HEAD
     const autoHeader = req.headers.authorization
          if(!autoHeader) {
             return res.status(401).send({
@@ -28,29 +27,6 @@ async function auth(req, res, next) {
       message: "Autenticação falhou!",
       cause: error.message
     })
-=======
-      let token = await req.headers['authorization'];
-      if (!token) return res.status(401).send('Acesso negado! Token não fornecido.');
-
-      console.log("Antes do slice: ", token);
-
-      if (token.startsWith('Bearer ')) {
-         token = token.slice(7);
-      }
-
-      console.log("depois do slice: ", token);
-
-      jwt.verify(token, process.env.SECRET_JWT, (err, user) => {
-         if (err) return res.status(403).send('Token inválido!');
-         req.user = user;
-         next();
-      });
-   } catch (error) {
-      return res.status(401).json({
-         message: "Autenticação falhou!",
-         cause: error.message
-      });
->>>>>>> 41b6eea153e669dbf4bf50b55d6efa1cce7969c7
    }
 }
 
